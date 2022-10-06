09dd1ec89a74fa8f9642b127493fa5dedfa74fbe
3virgula14 <3virgula14de96@gmail.com>
2022-10-06 16:34:11 -0300

    Changes to be committed: 	new file:   src/components/temppage/index.jsx 	modified:   src/routes/index.tsx

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,10 +1,11 @@
 import { component$ } from '@builder.io/qwik';
 import type { DocumentHead } from '@builder.io/qwik-city';
+import TempPage from '../components/temppage'
 
 export default component$(() => {
   return (
     <div id="content">
-      conteúdo do index
+      <TempPage/>
     </div>
   );
 });
