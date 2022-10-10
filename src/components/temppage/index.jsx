5c741b2950a7fb0aae7bfde80004c12a41eda6ed
3virgula14 <3virgula14de96@gmail.com>
2022-10-09 22:32:58 -0300

    Changes to be committed: 	modified:   src/components/temppage/index.css 	modified:   src/components/temppage/index.jsx

--- src/components/temppage/index.jsx
+++ src/components/temppage/index.jsx
@@ -1,14 +1,12 @@
 
 import { component$ } from '@builder.io/qwik';
 import './index.css';
+import PostData from 'public/_posts/portfolio_items/Teste-2022-10-09.json'
 
 export default component$(() => {
   return (
-    <div id="temp_page" style="">
-        <img src="/favicon.png" alt="" />
-        <span>
-          Em construção
-        </span>
-    </div>
-  );
-});
+  <div id="temp_page" style="">
+    <img src={PostData.photos[0].image} alt="" />
+  </div>
+  )
+})
