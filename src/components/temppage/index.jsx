5331711a3d7c5d2fe30960182032aa5d7e6289e3
3virgula14 <3virgula14de96@gmail.com>
2022-10-09 23:26:44 -0300

    /

--- src/components/temppage/index.jsx
+++ src/components/temppage/index.jsx
@@ -1,12 +1,12 @@
 
 import { component$ } from '@builder.io/qwik';
 import './index.css';
-import PostData from 'public/_posts/portfolio_items/Teste-2022-10-09.json'
+// import PostData from 'public/_posts/portfolio_items/Teste-2022-10-09.json'
 
 export default component$(() => {
   return (
   <div id="temp_page" style="">
-    <img src={PostData.photos[0].image} alt="" />
+    {/* <img src={PostData.photos[0].image} alt="" /> */}
   </div>
   )
 })
