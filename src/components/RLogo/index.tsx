1bcc438b2abd771a0f9bfceaf789da9947bd3058
3virgula14 <110666575+3virgula14@users.noreply.github.com>
2022-10-17 06:58:37 -0300

    Update index.tsx

--- src/components/RLogo/index.tsx
+++ src/components/RLogo/index.tsx
@@ -13,6 +13,12 @@ export interface RLogoProps{
     location: string
 }
 
+ interface NavigatorStateStruct {
+    dataIndex: number,
+    dataIndexAttribute: string,
+    dataType: string
+  }
+
 import "./styles.css";
 
 export default  component$((props: RLogoProps) => {   
@@ -40,6 +46,9 @@ export default  component$((props: RLogoProps) => {
         setTimeout(()=>{LogoRestMode.activated=false}, 2000)
         return `animate__slideInDown ${LogoRestMode.activated? "activated":""}`
     }
+    
+
+    let NavigatorLocalState:NavigatorStateStruct = useContext(NavigationContext) as NavigatorStateStruct;
 
     return (
         <div
@@ -53,12 +62,12 @@ export default  component$((props: RLogoProps) => {
                         gap: "3px",
                         pointerEvents: "all"}}>
 
-                    <a href={`${(driverParam2 ("-", 1))}`}>
+                    <a onClick$={() => {NavigatorLocalState.dataIndex = driverParam2 ("-", 1)} }>
                         <svg style={{transform: "rotate(180deg)"}} class="wh-24" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width={2} d="M17 8l4 4m0 0l-4 4m4-4H3" /></svg>
                     </a>
                     <span>{driverParam2()}/{props.data.length}</span>
                     
-                    <a href={`${(driverParam2 ("+", 1))}`}>
+                    <a onClick$={() => {NavigatorLocalState.dataIndex = driverParam2 ("+", 1)} }>
                         <svg class="wh-24" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width={2} d="M17 8l4 4m0 0l-4 4m4-4H3" /></svg>
                     </a>
                 </span>
