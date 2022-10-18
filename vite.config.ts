45b636f2579991fd7900007fa2a01695363957ee
3virgula14 <110666575+3virgula14@users.noreply.github.com>
2022-10-18 06:22:55 -0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -2,18 +2,14 @@ import { defineConfig } from "vite";
 import { qwikVite } from "@builder.io/qwik/optimizer";
 import { qwikCity } from "@builder.io/qwik-city/vite";
 import tsconfigPaths from "vite-tsconfig-paths";
-import netlifyEdge from "@netlify/vite-plugin-netlify-edge";
 
 export default defineConfig(() => {
   return {
     ssr: { target: "node", format: "cjs" },
     plugins: [
       qwikCity(),
-      qwikVite({
-        ssr: { outDir: "netlify/edge-functions/entry.netlify-edge" },
-      }),
+      qwikVite(),
       tsconfigPaths(),
-      netlifyEdge({ functionName: "entry.netlify-edge" }),
     ],
   };
 });
